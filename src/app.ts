ab9907506e9c0965aa560fc3922fcae04ffbf144
Mikhail Dudin <dudin-m@live.ru>
2020-02-16 21:15:30 +0300

    Remove app.ts

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import * as path from 'path';
-import {TextGenerator} from './textGenerator';
-
-const corporaPath = path.join(__dirname, '../corpora.txt');
-const generator = new TextGenerator(corporaPath);
-
-console.log(generator.generate());
